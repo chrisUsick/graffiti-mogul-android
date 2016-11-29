04d85df06d2f28260c4eaa9a2fa8b427d5eff691
Chris Usick <christopher.usick@gmail.com>
2016-11-29 15:54:45 -0600

    geocoding tag name

--- app/src/main/java/com/cu_dev/graffitimogul/TagLocationActivity.java
+++ app/src/main/java/com/cu_dev/graffitimogul/TagLocationActivity.java
@@ -1,26 +1,55 @@
 package com.cu_dev.graffitimogul;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TagLocationActivity extends AppCompatActivity {
+public class TagLocationActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "TAG_LOCATION_ACTIVITY";
+    private static final int PERM_REQUEST_LOCATION = 1;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_location);
 
         setImageView(extractBitmap());
+        mGoogleApiClient = null;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+
     }
 
     private void setImageView(Bitmap bitmap) {
@@ -41,5 +70,60 @@ public class TagLocationActivity extends AppCompatActivity {
     public void submitClick(View v) {
         Toast.makeText(this, "Uploading thumbnail", Toast.LENGTH_SHORT).show();
         finish();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "Checking Permission");
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERM_REQUEST_LOCATION);
+        } else {
+            Log.d(TAG, "getting location name");
+            Geocoder geocoder = new Geocoder(this);
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+            if (mLastLocation != null) {
+                try {
+                    List<Address> addresses =
+                            geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(),1);
+                    Address address = addresses.get(0);
+                    TextView nameView = (TextView)findViewById(R.id.name);
+                    if (address != null) {
+                        Log.d(TAG, "Found Address: " + address.toString());
+                        nameView.setText(address.getAddressLine(0));
+                    } else {
+                        nameView.setText("Lat Lon: + " + mLastLocation.getLatitude() + ", " + mLastLocation.getLongitude());
+                    }
+                } catch (IOException e) {
+                    Log.d(TAG, "IO Exception in get geocode", e);
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection to google api builder failed");
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 }
--- app/src/main/java/com/cu_dev/graffitimogul/web/FetchTags.java
+++ app/src/main/java/com/cu_dev/graffitimogul/web/FetchTags.java
@@ -2,15 +2,25 @@ package com.cu_dev.graffitimogul.web;
 
 import com.cu_dev.graffitimogul.domain.Tag;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by chris on 03-Nov-16.
  */
 
 public class FetchTags extends AbstractRequest<FetchTags.Options, List<Tag>> {
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
     static public class Options {
         public Options() {
 
